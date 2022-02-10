988ad87081abe0f5afc3232a60c51efc827f9ed6
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2022-02-10 16:38:30 -0500

    subtitle

--- subtitle.py
+++ subtitle.py
@@ -48,6 +48,7 @@ def get_subtitle(args, file):
         cmd(
             f"yt-dlp --write-sub --write-auto-sub --sub-lang en --sub-format srt/sub/ssa/vtt/ass/best --skip-download https://youtu.be/{yt_video_id}",
             cwd=str(Path(file).resolve().parent),
+            strict=False,
         )
 
     if not args.youtube:
