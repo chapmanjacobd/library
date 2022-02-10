0a88b449d3b6ae0b8259f548b606be9cecf44a92
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2022-02-10 17:52:04 -0500

    watch

--- watch.py
+++ watch.py
@@ -88,7 +88,7 @@ def main():
     print(next_video)
 
     if os.path.exists(next_video) and "/keep/" not in next_video:
-        cmd(f"mpv {quote(next_video)} --fs")
+        cmd(f"mpv {quote(next_video)} --fs --audio-display=embedded-first")
         if args.keep and Confirm.ask("Keep?", default=False):
             keep_path = str(Path(next_video).parent / "keep/")
             cmd(f"mkdir -p {keep_path} && mv {quote(next_video)} {quote(keep_path)}")
