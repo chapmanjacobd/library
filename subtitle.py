6bae7233846642a777c96e7a31972fb247c2cb30
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2022-02-10 22:28:10 -0500

    subtitle

--- subtitle.py
+++ subtitle.py
@@ -15,7 +15,7 @@ load_dotenv(dotenv_path=Path(".") / ".env")
 def ytdl_id(file) -> str:
     if len(file) < 15:
         return ""
-    # rename old ytdl format to new one: cargo install renamer; fd -tf . -x renamer '\-([\w\-_]{11})= [$1]' {}
+    # rename old ytdl format to new one: cargo install renamer; fd -tf . -x renamer '\-([\w\-_]{11})\.= [$1].' {}
     idregx = re.compile(r"-([\w\-_]{11})\..*$|\[([\w\-_]{11})\]\..*$", flags=re.M)
     file = str(file).strip()
 
