da88e6a8daa9bd9cf9b274cad6f802f434a746cd
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2022-04-18 00:06:48 -0500

    duplicate_music find_duplicates

--- duplicate_music.py
+++ duplicate_music.py
@@ -1,3 +1,15 @@
 import geopandas as gpd
 
-DF = gpd.read_file("audio.db")
+DF = gpd.read_file("audio2.db")
+
+DF["NUQ"] = (
+    DF.filename.replace(r"\s", "", regex=True)
+    .replace(r"\-|\_|\]|\[", "", regex=True)
+    .replace(r"\.\d\d\..*$", ".", regex=True)
+    .replace(r"\..*$", "", regex=True)
+    .replace(r"\/mnt\/d\/.*\/", "", regex=True)
+)
+
+DF["dupe"] = DF["NUQ"].duplicated(keep=False)
+
+DF.to_file("audio4.db", driver="SQLite")
