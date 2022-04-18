f055be73b12d7fe64f56d2c97cde880d1fe2109a
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2022-04-18 00:06:48 -0500

    duplicate_music find_duplicates

--- find_duplicates.sql
+++ find_duplicates.sql
@@ -1,8 +1,16 @@
-select * from media mp3, media ogg
-where mp3.format_name !='ogg' and ogg.format_name = 'ogg'
+select
+mp3.filename,
+mp3.filesize,
+mp3.format_name,
+mp3.duration,
+ogg.filename,
+ogg.filesize,
+ogg.format_name,
+ogg.duration
+from media mp3, media ogg
+where mp3.fid < ogg.fid
+--and mp3.format_name !='ogg' and ogg.format_name = 'ogg'
 and cast(mp3.duration as int) >= cast(ogg.duration as int) - 2
 and cast(mp3.duration as int) <= cast(ogg.duration as int) + 2
-and replace(replace(replace(replace(replace(mp3.filename,' ',''), '-', ''),'_',''),'[',''),']','') = 
-    replace(replace(replace(replace(replace(ogg.filename,' ',''), '-', ''),'_',''),'[',''),']','')
-limit 5
-;
+and mp3.NUQ = ogg.NUQ and mp3.title = ogg.title and mp3.artist=ogg.artist and mp3.album=ogg.album
+--limit 5
