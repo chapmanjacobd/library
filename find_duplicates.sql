dd0e289f194e55d61928f9992c1d1ad2209caa5b
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2022-04-18 11:00:26 -0500

    gitignore find_duplicates

--- find_duplicates.sql
+++ find_duplicates.sql
@@ -7,9 +7,10 @@ ogg.filename,
 ogg.filesize,
 ogg.format_name,
 ogg.duration
-from media mp3, media ogg
-where mp3.fid < ogg.fid
---and mp3.format_name !='ogg' and ogg.format_name = 'ogg'
+from audio4 mp3, audio4 ogg
+where mp3.ogc_fid < ogg.ogc_fid
+--and mp3.format_name !='ogg' 
+and ogg.format_name = 'ogg'
 and cast(mp3.duration as int) >= cast(ogg.duration as int) - 2
 and cast(mp3.duration as int) <= cast(ogg.duration as int) + 2
 and mp3.NUQ = ogg.NUQ and mp3.title = ogg.title and mp3.artist=ogg.artist and mp3.album=ogg.album
