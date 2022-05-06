87bc5b1a931d69b0181d0696debf936f56f91908
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2022-05-06 18:47:21 -0500

    utils

--- utils.py
+++ utils.py
@@ -74,7 +74,11 @@ log = argparse_log()
 
 
 def cmd(command, strict=True, cwd=None, quiet=False):
-    lines_to_filter = ["  Stream #0:0: Audio: opus, 48000 Hz, stereo, fltp", "    Metadata:"]
+    lines_to_filter = [
+        "Stream #0:0: Audio: opus, 48000 Hz, stereo, fltp",
+        "Stream #0:0(eng): Audio: opus, 48000 Hz, stereo, fltp",
+        "Metadata:",
+    ]
 
     def filter_output(string):
         filtered_strings = []
