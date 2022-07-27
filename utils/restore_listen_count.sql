61e577a35347a7d37da2fbe936f2d097f307c066
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2022-07-27 14:16:30 -0500

    wip

--- utils/restore_listen_count.sql
+++ utils/restore_listen_count.sql
@@ -1,8 +1,21 @@
-attach 'audio.db' as audio;
-attach 'audio.db.bak' as audiobak;
-UPDATE audio.media
-    set listen_count = b.listen_count
-from audio.media a
-    inner join
-        audiobak.media b on b.filename = a.filename
-    where b.listen_count > 0;
+attach '/home/xk/lb/audio.db' AS audio;
+attach '/home/xk/lb/audio.db.bak' AS audiobak;
+UPDATE
+    audio.media SET listen_count = (
+        SELECT
+            listen_count
+        FROM
+            audiobak.media
+        WHERE
+            audio.media.filename = audiobak.media.filename
+    )
+WHERE
+    EXISTS(
+        SELECT
+            1
+        FROM
+            audiobak.media
+        WHERE
+            audio.media.filename = audiobak.media.filename
+            AND audiobak.media.listen_count > 0
+    );
