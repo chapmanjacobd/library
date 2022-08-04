afe6d6978094dbe04398a90bb3feebded80d4819
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2022-08-03 20:59:19 -0500

    wip

--- xklb/db.py
+++ xklb/db.py
@@ -1,7 +1,14 @@
+import os
 import sqlite3
 
+from xklb.utils import stop
 
-def sqlite_con(db="./videos.db"):
+
+def sqlite_con(db):
+    if not os.path.exists(db):
+        print('Database file does not exist. Create one first with lb-extract.')
+        stop()
+
     con = sqlite3.connect(db)
     con.row_factory = sqlite3.Row
     return con
