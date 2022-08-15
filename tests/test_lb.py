13738a40e26a68b8a7d5ed6c5b1b60eed0c01679
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2022-08-14 19:11:24 -0500

    wtf

--- tests/test_lb.py
+++ tests/test_lb.py
@@ -9,15 +9,18 @@ from xklb.fs_actions import watch as wt
 from xklb.fs_extract import main as xr
 from xklb.lb import lb
 
-db = "--db", "tests/data/test.db"
+v_db = "--db", "tests/data/video.db"
+a_db = "--db", "tests/data/audio.db"
 
-xr([*db, "tests/data/"])
+xr([*v_db, "tests/data/"])
+xr([*a_db, "--audio", "tests/data/"])
 
 
 def test_lb_help(capsys):
-    lb_help_text = "usage:,extract,xr,subtitle,sub,listen,lt,watch,wt,filesystem,fs,tubelist,playlist,playlists,tubeadd,ta,tubeupdate,tu,tubewatch,tw,entries,tubelisten,tl".split(
+    lb_help_text = "usage:,extract,xr,listen,lt,watch,wt,filesystem,fs,tubelist,playlist,playlists,tubeadd,ta,tubeupdate,tu,tubewatch,tw,tubelisten,tl".split(
         ","
     )
+    sys.argv = ["lb"]
     lb()
     captured = capsys.readouterr().out
     for help_text in lb_help_text:
@@ -32,7 +35,7 @@ def test_lb_help(capsys):
 def test_wt_help(capsys):
     wt_help_text = "usage:,WHERE,SORT,--duration".split(",")
 
-    sys.argv[1:] = ["-h"]
+    sys.argv = ["wt", "-h"]
     with pytest.raises(SystemExit):
         wt()
     captured = capsys.readouterr().out.replace("\n", "")
@@ -48,47 +51,48 @@ def test_wt_help(capsys):
 
 def test_wt_print(capsys):
     with pytest.raises(SystemExit):
-        lb(["wt", *db, "-p", "a"])
+        lb(["wt", *v_db, "-p", "a"])
     captured = capsys.readouterr().out.replace("\n", "")
     assert "Aggregate" in captured
 
     with pytest.raises(SystemExit):
-        lb(["wt", *db, "-pa"])
+        lb(["wt", *v_db, "-pa"])
     captured = capsys.readouterr().out.replace("\n", "")
     assert "Aggregate" in captured
 
     with pytest.raises(SystemExit):
-        lb(["wt", *db, "-p"])
+        lb(["wt", *v_db, "-p"])
     captured = capsys.readouterr().out.replace("\n", "")
     assert "Aggregate" not in captured
-    # assert "tests/data/test.mp4" in captured
 
 
 class TestLB(unittest.TestCase):
     @mock.patch("xklb.fs_actions.play")
     def test_lb_fs(self, play_mocked):
         for subcommand in ["watch", "wt"]:
-            lb([subcommand, *db])
+            lb([subcommand, *v_db])
             out = play_mocked.call_args[0][1].to_dict(orient="records")
-            assert out == [
-                {
-                    "path": "/home/xk/github/xk/lb/tests/data/test.mp4",
-                    "duration": 12,
-                    "subtitle_count": 0,
-                    "size": 135178,
-                }
-            ]
+            assert len(out) == 1
+            assert "tests/data/test.mp4" in out[0]["path"]
+            assert out[0]["duration"] == 12
+            assert out[0]["subtitle_count"] == 0
+            assert out[0]["size"] == 135178
 
-        sys.argv[1:] = db
+        sys.argv[1:] = v_db
         wt()
         out = play_mocked.call_args[0][1].to_dict(orient="records")
-        assert out == [
-            {"path": "/home/xk/github/xk/lb/tests/data/test.mp4", "duration": 12, "subtitle_count": 0, "size": 135178}
-        ]
+        assert len(out) == 1
+        assert "tests/data/test.mp4" in out[0]["path"]
+        assert out[0]["duration"] == 12
+        assert out[0]["subtitle_count"] == 0
+        assert out[0]["size"] == 135178
 
-        lb(["listen", *db])
+        lb(["listen", *a_db])
         out = play_mocked.call_args[0][1].to_dict(orient="records")
-        assert out == [{"path": "/home/xk/github/xk/lb/tests/data/test.mp4", "duration": 12, "size": 135178}]
+        assert len(out) == 2
+        assert "tests/data/test.opus" in out[0]["path"]
+        assert out[0]["duration"] == 8
+        assert out[0]["size"] == 135550
 
     # @mock.patch("xklb.fs_actions.play")
     # def test_wt_sort(self, play_mocked):
