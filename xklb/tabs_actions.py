1b391101085654527d6de76a68141238e9d61d90
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2022-09-02 17:47:28 -0500

    1.15.020

--- xklb/tabs_actions.py
+++ xklb/tabs_actions.py
@@ -47,26 +47,26 @@ def construct_tabs_query(args):
     query = f"""SELECT path
         , frequency
         , CASE
-            WHEN frequency = 'daily' THEN cast(STRFTIME('%s', datetime( time_played, 'unixepoch', '+1 Day' )) as int)
-            WHEN frequency = 'weekly' THEN cast(STRFTIME('%s', datetime( time_played, 'unixepoch', '+7 Days' )) as int)
-            WHEN frequency = 'monthly' THEN cast(STRFTIME('%s', datetime( time_played, 'unixepoch', '+1 Month' )) as int)
-            WHEN frequency = 'quarterly' THEN cast(STRFTIME('%s', datetime( time_played, 'unixepoch', '+3 Months' )) as int)
-            WHEN frequency = 'yearly' THEN cast(STRFTIME('%s', datetime( time_played, 'unixepoch', '+1 Year' )) as int)
+            WHEN frequency = 'daily' THEN cast(STRFTIME('%s', datetime( time_played, 'unixepoch', '+1 Day', '-3 hours' )) as int)
+            WHEN frequency = 'weekly' THEN cast(STRFTIME('%s', datetime( time_played, 'unixepoch', '+7 Days', '-3 hours' )) as int)
+            WHEN frequency = 'monthly' THEN cast(STRFTIME('%s', datetime( time_played, 'unixepoch', '+1 Month', '-3 hours' )) as int)
+            WHEN frequency = 'quarterly' THEN cast(STRFTIME('%s', datetime( time_played, 'unixepoch', '+3 Months', '-3 hours' )) as int)
+            WHEN frequency = 'yearly' THEN cast(STRFTIME('%s', datetime( time_played, 'unixepoch', '+1 Year', '-3 hours' )) as int)
         END time_valid
         {', ' + ', '.join(args.cols) if args.cols else ''}
     FROM media
     WHERE 1=1
         {args.sql_filter}
-        {"and date(time_valid, 'unixepoch') < date()" if not args.print else ''}
+        {"and time_valid < STRFTIME('%s', datetime())" if not args.print else ''}
     ORDER BY 1=1
         {', ' + args.sort if args.sort else ''}
+        {', time_played, time_valid, path' if args.print else ''}
         , play_count
         , frequency = 'daily' desc
         , frequency = 'weekly' desc
         , frequency = 'monthly' desc
         , frequency = 'quarterly' desc
         , frequency = 'yearly' desc
-        {', path' if args.print else ''}
         , ROW_NUMBER() OVER ( PARTITION BY
             play_count
             , frequency
