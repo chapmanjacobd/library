45ed783d4d371c503f2c0a309b5bad7992851f57
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2022-09-12 19:07:49 -0500

    fine-tune duration calculation to correspond with 20 minute average of my other media

--- xklb/fs_actions.py
+++ xklb/fs_actions.py
@@ -615,7 +615,7 @@ def construct_fs_query(args):
     query = f"""SELECT path
         , size
         {', duration' if args.action in [SC.listen, SC.watch] else ''}
-        {', cast(length(tags) / 5.0 / 220 * 60 as INT) + 10 duration' if args.action == SC.read else ''}
+        {', cast(length(tags) / 4.2 / 220 * 60 as INT) + 10 duration' if args.action == SC.read else ''}
         {', subtitle_count' if args.action == SC.watch else ''}
         {', sparseness' if args.action == SC.filesystem else ''}
         {', is_dir' if args.action == SC.filesystem else ''}
