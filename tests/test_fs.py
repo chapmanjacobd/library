de1ef56d71a817225d54935e60bc8287c9e13a88
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2022-09-15 02:07:07 -0500

    1.16.020

---
+++ tests/test_fs.py
@@ -0,0 +1,104 @@
+import sys, unittest
+from unittest import mock
+
+import pytest
+
+from xklb.fs_actions import watch as wt
+from xklb.fs_extract import main as xr
+from xklb.lb import lb
+
+v_db = "--db", "tests/data/video.db"
+xr([*v_db, "--optimize", "tests/data/"])
+
+a_db = "--db", "tests/data/audio.db"
+xr([*a_db, "--audio", "tests/data/"])
+
+
+def test_lb_help(capsys):
+    lb_help_text = "local media subcommands:,online media subcommands:".split(",")
+    sys.argv = ["lb"]
+    lb()
+    captured = capsys.readouterr().out
+    for help_text in lb_help_text:
+        assert help_text in captured
+
+    lb(["-h"])
+    captured = capsys.readouterr().out
+    for help_text in lb_help_text:
+        assert help_text in captured
+
+
+def test_wt_help(capsys):
+    wt_help_text = "usage:,where,sort,--duration".split(",")
+
+    sys.argv = ["wt", "-h"]
+    with pytest.raises(SystemExit):
+        wt()
+    captured = capsys.readouterr().out.replace("\n", "")
+    for help_text in wt_help_text:
+        assert help_text in captured
+
+    with pytest.raises(SystemExit):
+        lb(["wt", "-h"])
+    captured = capsys.readouterr().out.replace("\n", "")
+    for help_text in wt_help_text:
+        assert help_text in captured
+
+
+def test_wt_print(capsys):
+    lb(["wt", *v_db, "-p", "a"])
+    captured = capsys.readouterr().out.replace("\n", "")
+    assert "Aggregate" in captured
+
+    lb(["wt", *v_db, "-pa"])
+    captured = capsys.readouterr().out.replace("\n", "")
+    assert "Aggregate" in captured
+
+    lb(["wt", *v_db, "-p"])
+    captured = capsys.readouterr().out.replace("\n", "")
+    assert "Aggregate" not in captured
+
+
+class TestFs(unittest.TestCase):
+    @mock.patch("xklb.fs_actions.play")
+    def test_lb_fs(self, play_mocked):
+        for SC in ["watch", "wt"]:
+            lb([SC, *v_db])
+            out = play_mocked.call_args[0][1]
+            assert "test.mp4" in out["path"]
+            assert out["duration"] == 12
+            assert out["subtitle_count"] == 3
+            assert out["size"] == 136057
+
+        sys.argv[1:] = v_db
+        wt()
+        out = play_mocked.call_args[0][1]
+        assert "test.mp4" in out["path"]
+        assert out["duration"] == 12
+        assert out["subtitle_count"] == 3
+        assert out["size"] == 136057
+
+        lb(["listen", *a_db])
+        out = play_mocked.call_args[0][1]
+        assert "test" in out["path"]
+
+    @mock.patch("xklb.fs_actions.play")
+    def test_wt_search(self, play_mocked):
+        sys.argv = ["wt", *v_db, "-s", "tests", "test AND data", "-E", "2", "-s", "test", "-E", "3"]
+        wt()
+        out = play_mocked.call_args[0][1]
+        assert out is not None
+
+    @mock.patch("xklb.fs_actions.play")
+    def test_wt_sort(self, play_mocked):
+        sys.argv = ["wt", *v_db, "-u", "duration"]
+        wt()
+        out = play_mocked.call_args[0][1]
+        assert out is not None
+
+    @mock.patch("xklb.fs_actions.play")
+    def test_wt_size(self, play_mocked):
+        sys.argv = ["wt", *v_db, "--size", "-1"]  # less than 1MB
+        wt()
+        out = play_mocked.call_args[0][1]
+        assert out is not None
---
+++ tests/test_tube.py
@@ -0,0 +1,81 @@
+import sys, unittest
+from unittest import mock
+
+from xklb.lb import lb
+from xklb.tube_actions import tube_watch
+from xklb.tube_extract import tube_add, tube_update
+
+tube_db = "--db", "tests/data/tube.db"
+tube_add(
+    [
+        *tube_db,
+        "--extra",
+        "--yt-dlp-config",
+        "TEST1=1 TEST2=2",
+        "https://youtube.com/playlist?list=PLVoczRgDnXDLWV1UJ_tO70VT_ON0tuEdm",
+    ]
+)
+
+
+def test_tw_print(capsys):
+    lb(["tw", *tube_db, "-p", "a"])
+    captured = capsys.readouterr().out.replace("\n", "")
+    assert "Aggregate" in captured
+
+    lb(["tw", *tube_db, "-pa"])
+    captured = capsys.readouterr().out.replace("\n", "")
+    assert "Aggregate" in captured
+
+    lb(["tw", *tube_db, "-p"])
+    captured = capsys.readouterr().out.replace("\n", "")
+    assert "Aggregate" not in captured
+
+
+class TestTube(unittest.TestCase):
+    @mock.patch("xklb.fs_actions.play")
+    def test_lb_fs(self, play_mocked):
+        for SC in ["tubewatch", "tw"]:
+            lb([SC, *tube_db])
+            out = play_mocked.call_args[0][1]
+            assert "https://www.youtube.com/watch?v=QoXubRvB6tQ" in out["path"]
+            assert out["duration"] == 28
+            assert out["title"] == "Most Epic Video About Nothing"
+            assert out["size"] == 4797012
+
+        sys.argv[1:] = tube_db
+        tube_watch()
+        out = play_mocked.call_args[0][1]
+        assert "https://www.youtube.com/watch?v=QoXubRvB6tQ" in out["path"]
+        assert out["duration"] == 28
+        assert out["title"] == "Most Epic Video About Nothing"
+        assert out["size"] == 4797012
+
+    @mock.patch("xklb.fs_actions.play")
+    def test_tw_search(self, play_mocked):
+        sys.argv = ["tw", *tube_db, "-s", "nothing"]
+        tube_watch()
+        out = play_mocked.call_args[0][1]
+        assert out is not None
+
+    @mock.patch("xklb.fs_actions.play")
+    def test_tw_sort(self, play_mocked):
+        sys.argv = ["tw", *tube_db, "-u", "duration"]
+        tube_watch()
+        out = play_mocked.call_args[0][1]
+        assert out is not None
+
+    @mock.patch("xklb.fs_actions.play")
+    def test_tw_size(self, play_mocked):
+        sys.argv = ["tw", *tube_db, "--size", "+1"]  # more than 1MB
+        tube_watch()
+        out = play_mocked.call_args[0][1]
+        assert out is not None
+
+    @mock.patch("xklb.tube_extract.process_playlist")
+    def test_tubeupdate(self, play_mocked):
+        tube_update([*tube_db, "--yt-dlp-config", "TEST2=4 TEST3=3"])
+        out = play_mocked.call_args[0][2]
+        assert out is not None
+        assert play_mocked.call_args[0][2]['TEST1'] == '1'
+        assert play_mocked.call_args[0][2]['TEST2'] == '4'
+        assert play_mocked.call_args[0][2]['TEST3'] == '3'
