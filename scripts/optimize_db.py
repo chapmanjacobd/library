869b3e141e929f0b3881f5878ff85ed438ff8627
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2022-10-03 03:23:05 -0500

    use fts :-D so much faster

---
+++ scripts/optimize_db.py
@@ -0,0 +1,17 @@
+import argparse
+from xklb import db
+from xklb import utils
+from xklb.utils import log
+
+def optimize_db():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--verbose", "-v", action="count", default=0)
+    parser.add_argument("--db", "-db", help=argparse.SUPPRESS)
+    parser.add_argument("database", nargs="?", default="video.db")
+    args = parser.parse_args()
+    if args.db:
+        args.database = args.db
+    args.db = db.connect(args)
+    log.info(utils.dict_filter_bool(args.__dict__))
+
+    db.optimize(args)
