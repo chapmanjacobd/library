9721f0db8ddb8ea8423086f714400fd020c5e959
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2022-10-11 11:48:33 -0500

    1.18.007

--- xklb/consts.py
+++ xklb/consts.py
@@ -98,8 +98,8 @@ def sanitize_url(args, path: str) -> str:
         subreddit = matches.groups()[0]
         return "https://old.reddit.com/r/" + subreddit + "/top/?sort=top&t=" + reddit_frequency(args.frequency)
 
-    if "m.youtube" in path:
-        return path.replace("m.youtube", "www.youtube")
+    if "/m." in path:
+        return path.replace("/m.", "/www.")
 
     return path
 
