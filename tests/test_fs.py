c2122bcc05b2537654cecbbb1c5d5a77722be589
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2022-10-14 14:53:13 -0500

    1.18.018

--- tests/test_fs.py
+++ tests/test_fs.py
@@ -15,20 +15,6 @@ a_db = "--db", "tests/data/audio.db"
 fs_add([*a_db, "--audio", "tests/data/"])
 
 
-def test_lb_help(capsys):
-    lb_help_text = "local media subcommands:,online media subcommands:".split(",")
-    sys.argv = ["lb"]
-    lb()
-    captured = capsys.readouterr().out
-    for help_text in lb_help_text:
-        assert help_text in captured
-
-    lb(["-h"])
-    captured = capsys.readouterr().out
-    for help_text in lb_help_text:
-        assert help_text in captured
-
-
 def test_wt_help(capsys):
     wt_help_text = "usage:,where,sort,--duration".split(",")
 
