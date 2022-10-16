b8281b645602bf943a741ffb4b2f86a6bae8d8cc
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2022-10-16 14:54:53 -0500

    1.18.026

--- scripts/streaming_tab_loader.py
+++ scripts/streaming_tab_loader.py
@@ -24,7 +24,7 @@ def parse_args() -> argparse.Namespace:
     """,
     )
     parser.add_argument("database", nargs="?")
-    parser.add_argument("--count", "-n", default=4, type=int)
+    parser.add_argument("--count", "-n", default=2, type=int)
     parser.add_argument("--target-hosts", "--target", default=None, help="Target hosts IP:Port")
     parser.add_argument("--verbose", "-v", action="count", default=0)
     args = parser.parse_args()
