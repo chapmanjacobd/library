b5eae1b936aac75b48890d9801a42e6fb334174c
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2022-11-19 23:37:08 -0600

    add some documentation

--- xklb/utils.py
+++ xklb/utils.py
@@ -182,7 +182,9 @@ def cmd(*command, strict=True, cwd=None, quiet=True, **kwargs) -> subprocess.Com
 
 def cmd_detach(*command, **kwargs) -> subprocess.CompletedProcess:
     # https://stackoverflow.com/questions/62521658/python-subprocess-detach-a-process
-    # if using with ffmpeg remember to run with `-nostdin`
+    # After lb closes, the detached process becomes a child of the init process aka daemonized (not connected to the terminal so they won't show up in the shell command `jobs`)
+    # If you shut down your computer often, you may want to open: `watch progress -wc ffmpeg` in another terminal so that you don't forget many things are in the background
+    # If using with ffmpeg remember to include ffmpeg's flag `-nostdin` in the command when calling this function
     stdout = os.open(os.devnull, os.O_WRONLY)
     stderr = os.open(os.devnull, os.O_WRONLY)
     stdin = os.open(os.devnull, os.O_RDONLY)
