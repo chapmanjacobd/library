c557c1b0b3d5dd35c97e682600337d72731bc51c
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2023-04-04 10:32:59 +0800

    1.24.016

--- scripts/relmv.py
+++ scripts/relmv.py
@@ -29,10 +29,11 @@ def _relmv(args, sources, dest):
     for source in sources:
         abspath = Path(source).expanduser().resolve()
 
+        rel_prefix = commonprefix([abspath, dest])
         try:
-            relpath = str(abspath.relative_to(commonprefix([abspath, dest])))
+            relpath = str(abspath.relative_to(rel_prefix))
         except ValueError:
-            relpath = str(abspath.relative_to(Path(commonprefix([abspath, dest])).parent))
+            relpath = str(abspath.relative_to(Path(rel_prefix).parent))
         target_dir = (dest / relpath).parent
 
         # remove duplicate path parts
@@ -40,21 +41,22 @@ def _relmv(args, sources, dest):
 
         if args.test:
             print("mv", shlex.quote(str(abspath)), shlex.quote(str(target_dir)))
-        else:
-            target_dir.mkdir(parents=True, exist_ok=True)
-            try:
-                new_path = target_dir / abspath.name
-                log.info("%s -> %s", abspath, new_path)
-                abspath.rename(new_path)
-            except OSError as e:
-                if e.errno == 18:  # cross-device move
-                    log.info("%s -d> %s", abspath, target_dir)
-                    utils.cmd_interactive("mv", abspath, target_dir)
-                elif e.errno == 39:  # target dir not empty
-                    log.info("%s -m> %s", abspath, target_dir)
-                    _relmv(args, abspath.glob("*"), dest)
-                else:
-                    raise e
+            continue
+
+        target_dir.mkdir(parents=True, exist_ok=True)
+        try:
+            new_path = target_dir / abspath.name
+            log.info("%s -> %s", abspath, new_path)
+            abspath.rename(new_path)
+        except OSError as e:
+            if e.errno == 18:  # cross-device move
+                log.info("%s ->d %s", abspath, target_dir)
+                utils.cmd_interactive("mv", abspath, target_dir)
+            elif e.errno == 39:  # target dir not empty
+                log.info("%s ->m %s", abspath, dest)
+                _relmv(args, abspath.glob("*"), dest)
+            else:
+                raise e
 
 
 def relmv() -> None:
