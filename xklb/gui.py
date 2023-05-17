eb78156bed3f2c04671090944c270d6edb978e53
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2023-05-17 09:27:40 -0500

    1.28.004

--- xklb/gui.py
+++ xklb/gui.py
@@ -1,3 +1,4 @@
+from pathlib import Path
 from typing import NoReturn, Tuple
 
 from xklb.utils import log
@@ -24,7 +25,7 @@ class MrSuperDialogue:
         style = Style(self.root)
         style.theme_use("clam")
 
-        photo = PhotoImage(file="assets/kotobago.png")
+        photo = PhotoImage(file=str(Path("assets/kotobago.png")))
         self.root.wm_iconphoto(True, photo)  # noqa: FBT003
         self.root.wm_attributes("-topmost", 1)
 
