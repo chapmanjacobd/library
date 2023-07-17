3bdc731d8968f4c4518ffd56e776e1f8bd9616b8
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2023-07-17 10:31:56 -0600

    2.2.038

--- tests/utils.py
+++ tests/utils.py
@@ -1,4 +1,21 @@
 import vcr
+from vcr.record_mode import RecordMode
 
-dvd = vcr.VCR(cassette_library_dir="tests/cassettes/", filter_query_parameters=["key"])
-# dvd.match_on = ["method", "scheme", "host", "port", "path"]
+
+def filter_query_param(r1, r2):
+    if r1 == r2:
+        return True
+
+    query1 = dict(r1.query)
+    query2 = dict(r2.query)
+
+    for k in ["key", "api_key"]:
+        query1.pop(k, None)
+        query2.pop(k, None)
+
+    return query1 == query2
+
+
+dvd = vcr.VCR(cassette_library_dir="tests/cassettes/", record_mode=RecordMode.NEW_EPISODES)
+dvd.register_matcher("filter_query", filter_query_param)
+dvd.match_on = ["method", "scheme", "host", "port", "path", "filter_query"]
