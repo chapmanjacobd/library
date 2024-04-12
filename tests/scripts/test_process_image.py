c2f9966422da9ef3a99e4abd6b8a659f872e6558
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2024-04-12 09:10:29 +0800

    2.6.003

--- tests/scripts/test_process_image.py
+++ tests/scripts/test_process_image.py
@@ -1,11 +1,13 @@
 from pathlib import Path
 from subprocess import CalledProcessError
+from unittest import skip
 
 import pytest
 
 from xklb.lb import library as lb
 
 
+@skip("ImageMagick dependency")
 def test_incomplete_file(temp_file_tree):
     file_tree = {"file.jpg": "4"}
     src1 = temp_file_tree(file_tree)
@@ -13,6 +15,7 @@ def test_incomplete_file(temp_file_tree):
         lb(["process-image", str(Path(src1, "file.jpg"))])
 
 
+@skip("ImageMagick dependency")
 def test_incomplete_file_delete(temp_file_tree):
     file_tree = {"file.jpg": "4"}
     src1 = temp_file_tree(file_tree)
