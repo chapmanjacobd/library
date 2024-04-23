4406f3d6609499f9ece683d48d4224f2067e0427
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2024-04-23 15:28:53 +0800

    2.6.020

--- .github/README.md
+++ .github/README.md
@@ -97,7 +97,7 @@ To stop playing press Ctrl+C in either the terminal or mpv
 <details><summary>List all subcommands</summary>
 
     $ library
-    xk media library subcommands (v2.6.019)
+    xk media library subcommands (v2.6.020)
 
     Create database subcommands:
     ╭───────────────┬──────────────────────────────────────────╮
--- tests/mediafiles/test_process_image.py
+++ tests/mediafiles/test_process_image.py
@@ -1,18 +1,33 @@
+import shutil, tempfile
 from pathlib import Path
 from shutil import which
-from subprocess import CalledProcessError
 
 import pytest
 
+from tests.utils import get_default_args
 from xklb.lb import library as lb
+from xklb.mediafiles.process_image import process_path
+from xklb.utils import arggroups, objects, processes
 
 
 @pytest.mark.skipif(not which("magick"), reason="requires magick")
-def test_incomplete_file(temp_file_tree):
-    file_tree = {"file.jpg": "4"}
-    src1 = temp_file_tree(file_tree)
-    with pytest.raises(CalledProcessError):
-        lb(["process-image", str(Path(src1, "file.jpg"))])
+def test_process_image():
+    temp_dir = tempfile.TemporaryDirectory()
+    input_path = shutil.copy("tests/data/test_frame.gif", temp_dir.name)
+
+    args = objects.NoneSpace(**get_default_args(arggroups.process_ffmpeg))
+    output_path = process_path(args, input_path)
+
+    assert output_path is not None
+    assert Path(output_path).suffix == ".avif"
+
+    out_probe = processes.FFProbe(output_path)
+    assert out_probe.video_streams[0]["codec_name"] == "av1"
+
+    try:
+        temp_dir.cleanup()
+    except Exception as e:
+        print(e)
 
 
 @pytest.mark.skipif(not which("magick"), reason="requires magick")
