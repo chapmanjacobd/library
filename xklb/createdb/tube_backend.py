b11d63585c66dc79b2fc775e12a2740b1d5254ec
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2024-05-11 20:30:20 +0800

    das kapital?

--- xklb/createdb/tube_backend.py
+++ xklb/createdb/tube_backend.py
@@ -382,8 +382,8 @@ def download(args, m) -> None:
             "fragment": lambda n: 0.04 * (2**n),
         },
         "outtmpl": {
-            'default': out_dir("%(id).220b.%(ext)s"),
-            'chapter': out_dir("%(id).220b.%(section_number)03d.%(ext)s"),
+            'default': out_dir("%(id).220B.%(ext)s"),
+            'chapter': out_dir("%(id).220B.%(section_number)03d.%(ext)s"),
         },
     }
 
