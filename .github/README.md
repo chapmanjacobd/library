6e0bf1b1d8e7efdc19e206cb55cb302838faf6d9
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2024-05-21 23:49:28 +0800

    2.8.030

--- .github/README.md
+++ .github/README.md
@@ -97,7 +97,7 @@ To stop playing press Ctrl+C in either the terminal or mpv
 <details><summary>List all subcommands</summary>
 
     $ library
-    library (v2.8.029; 74 subcommands)
+    library (v2.8.030; 74 subcommands)
 
     Create database subcommands:
     ╭───────────────┬──────────────────────────────────────────╮
---
+++ tests/tablefiles/test_incremental_diff.py
@@ -0,0 +1,27 @@
+import pytest
+
+from xklb.lb import library as lb
+
+
+@pytest.mark.parametrize(
+    "args,stdout",
+    [
+        (
+            ["--batch-size=inf", "tests/data/test.xml", "tests/data/test2.xml"],
+            """## Diff tests/data/test.xml:0 and tests/data/test2.xml:0
+
+|    |   index |   A |   B |   C | _merge     |
+|----|---------|-----|-----|-----|------------|
+|  0 |       0 |   1 |   3 |   5 | left_only  |
+|  1 |       1 |   2 |   4 |   6 | left_only  |
+|  2 |       0 |   1 |   4 |   5 | right_only |
+|  3 |       1 |   2 |   4 |   3 | right_only |
+
+""",
+        ),
+    ],
+)
+def test_lb_incremental_diff(args, stdout, capsys):
+    lb(["incremental-diff", *args])
+    captured = capsys.readouterr().out
+    assert stdout in captured
