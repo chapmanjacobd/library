d3209897075d07156ad57fea7ba68994f611153c
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2024-05-23 17:28:03 +0800

    2.8.039

--- tests/utils/test_web.py
+++ tests/utils/test_web.py
@@ -2,7 +2,7 @@ import pytest
 from bs4 import BeautifulSoup
 
 from tests.utils import p
-from xklb.utils.web import extract_nearby_text, safe_unquote, url_to_local_path
+from xklb.utils.web import extract_nearby_text, safe_unquote, url_encode, url_to_local_path
 
 
 def test_url_to_local_path():
@@ -120,33 +120,70 @@ def test_url_to_local_path_with_response(url, output_path, output_prefix, respon
     assert p(result) == p(expected), f"Failed for URL: {url}"
 
 
-@pytest.mark.parametrize(
-    "test_input,expected",
-    [
-        (
-            "http://example.com/some%20path;param%3Dvalue?query=value%23with%23hashes#fragment%2Fpart",
-            "http://example.com/some path;param=value?query=value%23with%23hashes#fragment/part",
-        ),
-        ("http://example.com/some%20path", "http://example.com/some path"),
-        (
-            "http://example.com/test?query%3Dvalue%26=another%3Dtest",
-            "http://example.com/test?query%3Dvalue%26=another%3Dtest",
-        ),
-        ("http://example.com/test?query=value&another=test", "http://example.com/test?query=value&another=test"),
-        ("http://example.com/test#fragment%2Fpart", "http://example.com/test#fragment/part"),
-        ("http://example.com/?q=a%26b", "http://example.com/?q=a%26b"),
-        (
-            "http://example.com/path%2Fto%2Fresource?search=foo%20bar%26baz%3Dqux#sec%2Ftion",
-            "http://example.com/path/to/resource?search=foo bar%26baz%3Dqux#sec/tion",
-        ),
-        (
-            "https://example.com/products?name=Widget%20Pro&details=color%3DBlue%26size%3DLarge&discount_code=SPRING20",
-            "https://example.com/products?name=Widget Pro&details=color%3DBlue%26size%3DLarge&discount_code=SPRING20",
-        ),
-    ],
-)
+unquote_results = [
+    (
+        "http://example.com/some%20path;param%3Dvalue?query=value%23with%23hashes#fragment%2Fpart",
+        "http://example.com/some path;param=value?query=value%23with%23hashes#fragment/part",
+    ),
+    ("http://example.com/some%20path", "http://example.com/some path"),
+    (
+        "http://example.com/test?query%3Dvalue%26=another%3Dtest",
+        "http://example.com/test?query%3Dvalue%26=another%3Dtest",
+    ),
+    ("http://example.com/test?query=value&another=test", "http://example.com/test?query=value&another=test"),
+    ("http://example.com/test#fragment%2Fpart", "http://example.com/test#fragment/part"),
+    ("http://example.com/?q=a%26b", "http://example.com/?q=a%26b"),
+    (
+        "http://example.com/path%2Fto%2Fresource?search=foo%20bar%26baz%3Dqux#sec%2Ftion",
+        "http://example.com/path/to/resource?search=foo bar%26baz%3Dqux#sec/tion",
+    ),
+    (
+        "https://example.com/products?name=Widget%20Pro&details=color%3DBlue%26size%3DLarge&discount_code=SPRING20",
+        "https://example.com/products?name=Widget Pro&details=color%3DBlue%26size%3DLarge&discount_code=SPRING20",
+    ),
+]
+
+
+@pytest.mark.parametrize("test_input,expected", unquote_results)
 def test_safe_unquote(test_input, expected):
     assert safe_unquote(test_input) == expected
+
+
+quote_results = [
+    (
+        "http://example.com/some path",
+        "http://example.com/some%20path",
+    ),
+    (
+        "http://example.com/test?query%3Dvalue%26=another%3Dtest",
+        "http://example.com/test?query%3Dvalue%26=another%3Dtest",
+    ),
+    (
+        "http://example.com/test?query=value&another=test",
+        "http://example.com/test?query=value&another=test",
+    ),
+    (
+        "http://example.com/?q=a%26b",
+        "http://example.com/?q=a%26b",
+    ),
+    (
+        "http://example.com/path/to/resource?search=foo bar%26baz%3Dqux",
+        "http://example.com/path/to/resource?search=foo%20bar%26baz%3Dqux",
+    ),
+    (
+        "http://example.com/some path;param=value?query=value%23with%23hashes#fragment/part",
+        "http://example.com/some%20path;param%3Dvalue?query=value%23with%23hashes#fragment/part",
+    ),
+    (
+        "https://example.com/products?name=Widget Pro&details=color%3DBlue%26size%3DLarge&discount_code=SPRING20",
+        "https://example.com/products?name=Widget%20Pro&details=color%3DBlue%26size%3DLarge&discount_code=SPRING20",
+    ),
+]
+
+
+@pytest.mark.parametrize("test_input, expected", quote_results)
+def test_safe_quote(test_input, expected):
+    assert url_encode(test_input) == expected
 
 
 html = """
