d920822cf52e45788d47fdd74a0bc3750fe01b46
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2024-08-28 15:30:26 +0000

    add test for lb search-db

---
+++ tests/fsdb/test_search_db.py
@@ -0,0 +1,8 @@
+from tests.utils import v_db
+from xklb.__main__ import library as lb
+
+def test_search_db(capsys):
+    lb(["sdb", v_db, "media", "test.gif"])
+    captured = capsys.readouterr().out
+    assert "image/gif" in captured.replace("\n", "")
+    assert len(captured) > 150
