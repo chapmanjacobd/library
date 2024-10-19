ad3daf262b2252e7c0e273f4fa32e53a39b4a2d7
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2024-10-20 00:22:08 +0800

    3.0.005

--- xklb/__main__.py
+++ xklb/__main__.py
@@ -5,7 +5,7 @@ from tabulate import tabulate
 from xklb.utils import argparse_utils, iterables
 from xklb.utils.log_utils import log
 
-__version__ = "3.0.004"
+__version__ = "3.0.005"
 
 progs = {
     "Create database subcommands": {
