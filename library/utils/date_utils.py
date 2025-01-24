d2ad8c5f4fe2cdb727943ba9dfbdadced7229c9b
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2025-01-24 04:01:53 +0000

    web-add: fix TypeError: can't compare offset-naive and offset-aware datetimes

--- library/utils/date_utils.py
+++ library/utils/date_utils.py
@@ -1,11 +1,21 @@
 import datetime
+from datetime import timezone as tz
 
 import dateutil.parser
 
 from library.utils import iterables, nums
 
+def is_tz_aware(dt: datetime.datetime) -> bool:
+    return dt.tzinfo is not None and dt.tzinfo.utcoffset(dt) is not None
 
-def super_parser(date_str):
+def maybe_tz_now(maybe_tz_dt: datetime.datetime):
+    if is_tz_aware(maybe_tz_dt):
+        now = datetime.datetime.now(tz=tz.utc).astimezone()
+    else:
+        now = datetime.datetime.now()
+    return now
+
+def super_parser(date_str, fallback=False):
     parsing_strategies = [
         {},  # default
         {"dayfirst": True},
@@ -19,10 +29,13 @@ def super_parser(date_str):
         except dateutil.parser.ParserError:
             continue
 
+    if fallback:
+        return date_str
+
 
 def specific_date(*dates):
     valid_dates = [super_parser(s) for s in dates if s]
-    past_dates = [d for d in valid_dates if d and d < datetime.datetime.now()]
+    past_dates = [d for d in valid_dates if d and d < maybe_tz_now(d)]
     if not past_dates:
         return None
 
