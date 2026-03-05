b081115bfe0ca407ef3a02390b25a4d96ace6533
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2026-03-05 04:21:51 +0000

    fix? url_encode aggressively encoding brackets

--- tests/playback/test_links_open.py
+++ tests/playback/test_links_open.py
@@ -18,3 +18,16 @@ def test_links_open(mock_souffle):
         {"path": "https://site1", "hostname": "site1", "category": "p1"},
         {"path": "https://site2", "hostname": "site2", "category": "p1"},
     ]
+
+@mock.patch("library.playback.links_open.make_souffle")
+@mock.patch("library.playback.links_open.play")
+def test_links_open_brackets(mock_play, mock_souffle, temp_db):
+    db_path = temp_db()
+    url = "https://example.com/test?page[]=107"
+    lb(["links-add", "--no-extract", db_path, url])
+    lb(["links-open", db_path, "-s", url])
+    
+    assert mock_souffle.called
+    media = mock_souffle.call_args[0][1]
+    assert len(media) == 1
+    assert media[0]["path"] == url
