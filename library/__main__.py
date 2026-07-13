bab9d4a6679be07bd7be0bb77e8c88c66d47addb
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2026-07-12 23:39:51 -0500

    3.0.175

--- library/__main__.py
+++ library/__main__.py
@@ -5,7 +5,7 @@ from tabulate import tabulate
 from library.utils import argparse_utils, iterables
 from library.utils.log_utils import log
 
-__version__ = "3.0.174"
+__version__ = "3.0.175"
 
 progs = {
     "Create database subcommands": {
