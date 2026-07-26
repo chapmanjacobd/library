7f4b49a1ec52845a10f5fe47b4e48b9878217e6f
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2026-07-26 00:33:03 -0500

    db_media.py: Modified 1 line

--- library/mediadb/db_media.py
+++ library/mediadb/db_media.py
@@ -29,7 +29,7 @@ def create(args):
         );
         """
     )
-    if args.action in ("tube-add", "gallery-add", "web-add", "site-add"):
+    if args.action in ("tube-add", "gallery-add", "web-add", "site-add", "computers-add"):
         args.db.execute("DROP INDEX IF EXISTS idx_media_path;")
         try:
             args.db.execute("CREATE UNIQUE INDEX IF NOT EXISTS media_uniq_path_idx ON media (playlists_id, path);")
